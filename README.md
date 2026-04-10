0375ebe5996ab13a65f1331b901b0916bb6320c8
oceane <oceane.gomez@esiroi.re>
2026-04-10 08:09:41 +0000

    fin de l'exercice 13

--- README.md
+++ README.md
@@ -16,3 +16,23 @@ Nous allons réimplémenter la librairie liste-c. Pour plus d'informations, suiv
 
 #### Lien du dépôt git d'origine de la librairie liste-c : 
 Vous trouverez le dépot git d'origine [ici](https://github.com/payetdenis/liste-c).
+
+## API de la librairie liste-c
+
+versionLC  : retourne une chaîne de caractères qui indique la version de la librairie.
+
+createLC : créer une liste vide. 
+
+sizeLC : retourne le nombre d’éléments dans la liste.
+
+addLC : ajouter une valeur à la liste à la fin.
+
+getLC : récupérer la valeur d’un élément à une position donnée dans la liste.
+
+replaceLC : remplacer la valeur d’un élément à une position donnée par une nouvelle valeur.
+
+insertLC : ajouter un élément à une position précise dans la liste. Les autres éléments sont décalés.
+
+removeLC : supprimer un élément à une position donnée dans la liste.
+
+deleteLC : supprimer toute la liste et de libérer la mémoire utilisée.
--- src/liste-c.h
+++ src/liste-c.h
@@ -1,16 +1,18 @@
 #pragma once
 #include <stdlib.h>
 
-char* versionLC(void);
+char* versionLC(void); // version de la librairie 
 
-struct st_listeC;
+struct st_listeC;    
 typedef struct st_listeC * ListeC;
 
-ListeC createLC();					
-int sizeLC(ListeC liste);
-void addLC(ListeC liste, int value);	
-int getLC(ListeC liste, int pos);	
+ListeC createLC();		// Crée une liste			
+int sizeLC(ListeC liste); // taille de la liste
+void addLC(ListeC liste, int value); // ajoute un élément à la liste
+int getLC(ListeC liste, int pos);	// récupère un élément à la position souhaitée
 
-void replaceLC(ListeC liste, int value, int pos);	
-void insertLC(ListeC liste, int value, int pos);	
-void removeLC(ListeC liste, int pos);
+void replaceLC(ListeC liste, int value, int pos);	// remplace une élément par un autre choisi par la position souhaitée
+void insertLC(ListeC liste, int value, int pos);	// insère un élément à une certaine position
+void removeLC(ListeC liste, int pos);                   // supprime un élément situé à une position souhaitée
+
+void deleteLC(ListeC liste); // Supprime toute la liste
